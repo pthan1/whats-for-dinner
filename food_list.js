56c98c5826bdd61645ff41fcf8bd985a3ea7377d
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-07-22 19:57:20 -0600

    Create food lists

---
+++ food_list.js
@@ -0,0 +1,48 @@
+var sides = [
+    Miso Glazed Carrots,
+    Coleslaw,
+    Garden Salad,
+    Crispy Potatoes,
+    Sweet Potato Tots,
+    Coconut Rice,
+    Caeser Salad,
+    Veggie Summer Rolls,
+    Garlic Butter Mushrooms,
+    Hush Puppies
+];
+
+var main = [
+    Spaghetti,
+    Shakshuka,
+    Thai Yellow Curry,
+    Bibimbap,
+    Eggplant Parmesan,
+    Butternut Squash Soup,
+    Veggie Burgers,
+    Ramen,
+    Empanadas,
+    Fried Rice,
+    Sheet Pan Fajitas,
+    Margarita Pizza
+];
+
+var dessert = [
+    Apple Pie,
+    Lemon Meringue Pie,
+    Black Forest Cake,
+    Banana Bread,
+    Peach Cobbler,
+    Cheesecake,
+    Funfetti Cake,
+    Baklava,
+    Flan,
+    Macarons,
+    Macaroons,
+    Chocolate Cupcakes,
+    Pavlova,
+    Pumpkin Pie,
+    Key Lime Pie,
+    Tart Tatin,
+    Croissants,
+    Eclairs
+];
